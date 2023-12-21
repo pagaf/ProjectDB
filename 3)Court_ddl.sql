4450e3700937a1aaca58a988769fe07ea63b12b0
Alexander <47082891+StrangeAlex@users.noreply.github.com>
2023-12-21 20:55:59 +0300

    Update 3)Court_ddl.sql

--- 3)Court_ddl.sql
+++ 3)Court_ddl.sql
@@ -15,13 +15,10 @@ CREATE TABLE court.Courtrooms (
 -- Таблица Cases
 DROP TABLE IF EXISTS Cases cascade;
 CREATE TABLE court.Cases (
-    case_id INTEGER,
+    case_id INTEGER PRIMARY KEY,
     article TEXT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE,
     status TEXT,
-    CONSTRAINT pk_cases PRIMARY KEY (case_id, from_date),
-    CONSTRAINT chk_to_date CHECK (to_date IS NULL OR to_date >= from_date)
+    start_date DATE NOT NULL
 );
 
 -- Таблица Meetings
@@ -32,15 +29,17 @@ CREATE TABLE court.Meetings (
     case_id INTEGER NOT NULL,
     meeting_date DATE NOT NULL,
     meeting_time TIME NOT NULL,
-    judge_id TEXT NOT NULL,
     court_decision TEXT NOT NULL,
+    CONSTRAINT fk_case FOREIGN KEY (case_id) REFERENCES court.Cases(case_id),
     CONSTRAINT fk_meetings_room FOREIGN KEY (room_no) REFERENCES court.Courtrooms(room_no)
 );
+
+
 
 -- Таблица People
 DROP TABLE IF EXISTS People cascade;
 CREATE TABLE court.People (
-    taxpayer_id TEXT PRIMARY KEY,
+    taxpayer_id NUMERIC(12, 0) PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     passport_series NUMERIC(4, 0) NOT NULL,
@@ -49,12 +48,12 @@ CREATE TABLE court.People (
     phone_number VARCHAR(20) CHECK (phone_number ~ '^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$'),
     home_address TEXT,
     CONSTRAINT chk_birth_date CHECK (birth_date IS NULL OR birth_date <= CURRENT_DATE)
-);
+    );
 
 -- Таблица Companies
 DROP TABLE IF EXISTS Companies cascade;
 CREATE TABLE court.Companies (
-    taxpayer_id TEXT PRIMARY KEY,
+    taxpayer_id NUMERIC(12, 0) PRIMARY KEY,
     name_of_comp TEXT NOT NULL,
     email TEXT NOT NULL,
     legal_address TEXT NOT NULL
@@ -64,13 +63,15 @@ CREATE TABLE court.Companies (
 -- Таблица Roles
 DROP TABLE IF EXISTS Roles cascade;
 CREATE TABLE court.Roles (
-    taxpayer_id TEXT NOT NULL,
+    taxpayer_id NUMERIC(12, 0) NOT NULL,
     case_id INTEGER NOT NULL,
     role TEXT NOT NULL CHECK (role IN ('Судья', 'Адвокат истца', 'Прокурор', 'Адвокат подсудимого', 'Присяжный', 'Подсудимый', 'Истец')),
-    CONSTRAINT pk_roles PRIMARY KEY (taxpayer_id, case_id)
+    from_date DATE NOT NULL,
+    to_date DATE,
+    CONSTRAINT chk_to_date CHECK (to_date IS NULL OR to_date >= from_date),
+    CONSTRAINT pk_roles PRIMARY KEY (taxpayer_id, case_id, from_date),
+    CONSTRAINT fk_case_id FOREIGN KEY (case_id) REFERENCES court.cases(case_id)
 );
-
-
 
 
 -- Таблица Docs
@@ -79,5 +80,6 @@ CREATE TABLE court.Docs (
     doc_id INTEGER PRIMARY KEY,
     case_id INTEGER NOT NULL,
     date DATE,
-    type TEXT NOT NULL
+    type TEXT NOT NULL,
+    CONSTRAINT fk_case_id FOREIGN KEY (case_id) REFERENCES court.cases(case_id)
 );
