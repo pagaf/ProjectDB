8d99606337c61b370b38f6fcd7fbfc2dc5f8d319
Alexander <47082891+StrangeAlex@users.noreply.github.com>
2023-12-21 21:05:53 +0300

    Update 5)cruds.sql

--- 5)cruds.sql
+++ 5)cruds.sql
@@ -1,20 +1,32 @@
 -- CRUDы
--- 1)
-INSERT INTO court.Roles (taxpayer_id, case_id, role) VALUES
-('123456234098', 2, 'Присяжный'),
-('987651667784', 2, 'Присяжный'),
-('112233446613', 2, 'Адвокат истца');
 
--- 2)
-SELECT taxpayer_id FROM court.Roles
-where role = 'Истец'
-group by taxpayer_id;
+-- ТАБЛИЦА COURTROOMS
 
--- 3)
-UPDATE court.Companies
-SET email = 'arttech@email.com'
-WHERE taxpayer_id = '3456789012';
+-- Вставка данных в таблицу Courtrooms
+INSERT INTO court.Courtrooms (room_no, floor, capacity) VALUES (101, 1, 50), (102, 2, 75);
 
--- 4)
-DELETE FROM court.Companies
-WHERE taxpayer_id = '1234567890';
+-- Выбор данных из таблицы Courtrooms
+SELECT room_no, capacity FROM court.Courtrooms;
+
+-- Обновление данных в таблице Courtrooms
+UPDATE court.Courtrooms SET capacity = 60 WHERE room_no = 101;
+
+-- Удаление данных из таблицы Courtrooms
+DELETE FROM court.Courtrooms WHERE room_no = 102;
+
+
+-- ТАБЛИЦА PEOPLE
+
+-- Вставка данных в таблицу People
+INSERT INTO court.People (taxpayer_id, first_name, last_name, passport_series, passport_number, birth_date, phone_number, home_address)
+VALUES (123456789012, 'Иван', 'Иванов', 1234, 567890, '1990-01-01', '+7 123-456-7890', 'г. Москва, ул. Пушкина, д. 10');
+
+-- Выбор данных из таблицы People
+SELECT * FROM court.People;
+
+-- Обновление данных в таблице People
+UPDATE court.People SET phone_number = '+7 987-654-3210' WHERE taxpayer_id = 123456789012;
+
+-- Удаление данных из таблицы People
+DELETE FROM court.People WHERE taxpayer_id = 123456789012;
+
