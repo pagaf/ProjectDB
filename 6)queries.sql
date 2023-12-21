88c257af9387ac1ca51565eac9284953d90046c6
Alexander <47082891+StrangeAlex@users.noreply.github.com>
2023-12-21 21:36:05 +0300

    Update 6)queries.sql

--- 6)queries.sql
+++ 6)queries.sql
@@ -9,6 +9,7 @@ HAVING COUNT(taxpayer_id) > 2;
 -- Согласно списку инсертов это будут судья, истец и подсудимый.
 -- Очевидно, некоторые люди участвовали в одинаковой роли в разных делах, поэтому эти повторы мы убираем
 
+
 -- 2) список компаний и количество дел, в которых они участвуют
 SELECT Companies.name_of_comp, COUNT(DISTINCT Roles.case_id) AS case_count
 FROM court.Companies
@@ -17,22 +18,29 @@ GROUP BY Companies.name_of_comp;
 -- пока что в базу дел внесена информация только о 2 компаниях, следовательно
 -- для них кол-во дел будет = 1, для остальных будет выведен 0
 
+
 -- 3) средний возраст всех участников дел
 SELECT ROUND(AVG(EXTRACT(YEAR FROM age(CURRENT_DATE, birth_date))), 1) AS average_age
 FROM court.People;
 -- калькулятор сказал, что с учетом округления это примерно 43,6 лет
+
 
 -- 4) топ-3 залов с наибольшей вместимостью
 SELECT room_no, capacity
 FROM court.Courtrooms
 ORDER BY capacity DESC
 LIMIT 3;
--- с учетом имеющихся инсертов это будет 50, 40, 35
+-- с учетом имеющихся инсертов это будет 60, 50, 40
 
--- 5) "Путь" каждого дела, т.е. список его статусов в порядке возрастания даты
-SELECT case_id, status, to_date,
-       ROW_NUMBER() OVER (PARTITION BY case_id ORDER BY COALESCE(to_date, CURRENT_DATE)) as row_num
-FROM court.Cases;
--- по каждому делу выведется список со статусами, последний статус (с наибольшим номером
--- и датой to_date = NULL) будет иметь наибольший номер
--- а самый первый этап, очевидно, для каждого из дел будет иметь номер 1
+
+-- 5) Получение даты последнего заседания для каждого дела
+SELECT case_id, MIN(meeting_date) OVER (PARTITION BY case_id) AS first_meeting_date,
+       MAX(meeting_date) OVER (PARTITION BY case_id) AS last_meeting_date
+FROM court.Meetings;
+
+
+-- 6) Топ наиболее активных залов суда по количеству проведенных заседаний
+SELECT room_no, COUNT(meeting_no) AS meetings_count,
+       RANK() OVER (ORDER BY COUNT(meeting_no) DESC) AS room_activity_rank
+FROM court.Meetings
+GROUP BY room_no;
