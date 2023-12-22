fa10d9f0b1f125b363263f835c34dc5b6a571119
vlcir <112485129+vlcir@users.noreply.github.com>
2023-12-22 18:34:18 +0300

    Update 7.sql

--- 7.sql
+++ 7.sql
@@ -1,25 +1,36 @@
 -- 1)Представление для маскирования email компаний
 CREATE OR REPLACE VIEW masked_companies AS
-SELECT taxpayer_id, name_of_comp, regexp_replace(email, '(.*)@', '***@') AS masked_email, legal_address
+SELECT 
+	taxpayer_id,
+	name_of_comp,
+	regexp_replace(email, '(.*)@', '***@') AS masked_email,
+	legal_address
 FROM court.Companies;
-
 
 -- 2)Представление для маскирования телефонных номеров физических лиц
 CREATE OR REPLACE VIEW masked_people AS
-SELECT taxpayer_id, first_name, last_name, passport_series, passport_number, birth_date,
-       regexp_replace(phone_number, '(\d{3})\d{4}(\d{2})', '\1****\2') AS masked_phone_number, home_address
+SELECT
+	taxpayer_id,
+	first_name,
+	last_name,
+	passport_series,
+	passport_number,
+	birth_date,
+	regexp_replace(phone_number, '(\d{3})\d{4}(\d{2})', '\1****\2') AS masked_phone_number,
+	home_address
 FROM court.People;
 
-
 --3) Маскирование ролей судьи и адвокатов в таблице Roles
-create OR replace VIEW masked_roles AS
+CREATE OR REPLACE VIEW masked_roles AS
 SELECT
     taxpayer_id,
     case_id,
     CASE
         WHEN role IN ('Судья', 'Адвокат истца', 'Адвокат подсудимого', 'Прокурор') THEN 'Юрист'
         ELSE role
-    END AS masked_role
+    END AS masked_role,
+    from_date,
+    to_date
 FROM court.Roles;
 
 --4) Представление для таблицы Courtrooms
@@ -34,6 +45,7 @@ CREATE OR REPLACE VIEW masked_cases AS
 SELECT
     case_id,
     REGEXP_REPLACE(article, '[0-9]+', '***') AS article,
+    start_date,
     status
 FROM court.Cases;
 
@@ -49,7 +61,7 @@ SELECT
 FROM court.Meetings;
 
 --7) Представление для таблицы Docs
-CREATE OR REPLACE VIEW meetings_view AS
+CREATE OR REPLACE VIEW docs_view AS
 SELECT
     doc_id,
     case_id,
